e821f8ca02ec9adfc62cca60e7a4cba2e80b8000
fbfb79 <reg@beoni.net>
2017-08-01 16:10:49 +0200

    Test of default storage strategy creating default needed working paths

--- test/TestH5PLibraryDefaultStorageStrategy.js
+++ test/TestH5PLibraryDefaultStorageStrategy.js
@@ -14,6 +14,40 @@ const TestVars = require('./SharedTestVars');
 
 describe('H5P Libraries Management:', async()=> {
 
+    it(`Checks that all needed default dirs to work 
+        are available after the H5PLibraryDefaultStorageStrategy
+        is initialized`,
+
+        async()=>
+        {
+            let result = false;
+            try{
+                const WORKING_PATH = './test/dirs';
+                const fs = H5PLib.FileSystemDAL;
+                const createdDirs = H5PLib.H5PLibraryDefaultStorageStrategy.getWorkingPaths();
+
+                new H5PLib.H5PLibraryDefaultStorageStrategy({
+                    basePath:WORKING_PATH
+                });
+
+                let allPathsAvailable = true;
+                allPathsAvailable = allPathsAvailable && await fs.isPathWritable(fs.getPath().join(WORKING_PATH,createdDirs.EDITOR_PATH));
+                allPathsAvailable = allPathsAvailable && await fs.isPathWritable(fs.getPath().join(WORKING_PATH,createdDirs.CACHED_ASSETS_PATH));
+                allPathsAvailable = allPathsAvailable && await fs.isPathWritable(fs.getPath().join(WORKING_PATH,createdDirs.EXPORTS_PATH));
+                allPathsAvailable = allPathsAvailable && await fs.isPathWritable(fs.getPath().join(WORKING_PATH,createdDirs.CONTENT_PATH));
+                allPathsAvailable = allPathsAvailable && await fs.isPathWritable(fs.getPath().join(WORKING_PATH,createdDirs.LIBRARIES_PATH));
+
+                result = allPathsAvailable;
+            }
+            catch (err){
+                console.error(err);
+            }
+
+            assert.equal(result,true);
+        }
+    );
+
+
     //Requires writable folder
     it('Saves h5p package from upload folder to libraries repository', async ()=> {
         let isFullyStored = false;
@@ -41,32 +75,35 @@ describe('H5P Libraries Management:', async()=> {
         assert.equal(isFullyStored,true);
     });
 
-    it('cacheAssets(): Get a list of files to merge and put in a cache', async()=>{
+    it(`Get a list of files to merge and put in a cache
+        to test the first behaviour related to caching system`,
 
-        const CACHING_TEST_PATH = 'CachingAssetsTest';
-        const path = H5PLib.FileSystemDAL.getPath();
+        async()=>
+        {
+            const CACHING_TEST_PATH = 'CachingAssetsTest';
+            const path = H5PLib.FileSystemDAL.getPath();
 
-        var data = {
-            scripts: [
-                {path:path.join(CACHING_TEST_PATH,'fileOne.js'), version: '1'},
-                {path:path.join(CACHING_TEST_PATH,'fileTwo.js'), version: '1'}
-            ],
-            styles: [
-                {path:path.join(CACHING_TEST_PATH,'fileThree.css'), version: '1'},
-                {path:path.join(CACHING_TEST_PATH,'fileFour.css'), version: '1'}
-            ]
+            let data = {
+                scripts: [
+                    {path:path.join(CACHING_TEST_PATH,'fileOne.js'), version: '1'},
+                    {path:path.join(CACHING_TEST_PATH,'fileTwo.js'), version: '1'}
+                ],
+                styles: [
+                    {path:path.join(CACHING_TEST_PATH,'fileThree.css'), version: '1'},
+                    {path:path.join(CACHING_TEST_PATH,'fileFour.css'), version: '1'}
+                ]
+            };
+
+            let storageStrat = new H5PLib.H5PLibraryDefaultStorageStrategy({
+                basePath:TestVars.TEST_DATA_PATH
+            });
+
+            let merge = await storageStrat.cacheAssets(data,'my-key');
+
+            let hasResult = (merge!==null);
+
+            assert.equal(hasResult,true);
         }
-
-
-        let storageStrat = new H5PLib.H5PLibraryDefaultStorageStrategy({
-            basePath:TestVars.TEST_DATA_PATH
-        });
-
-        let merge = await storageStrat.cacheAssets(data,'my-key');
-
-        let hasResult = (merge!==null);
-
-        assert.equal(hasResult,true);
-    });
+    );
 
 });
